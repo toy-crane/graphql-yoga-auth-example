bf30a3be55d2dcf87530893684b1eedc44a7439d
hanwool kim <alwaysfun2183@gmail.com>
2020-04-01 18:04:48 +0900

    init graphql-yoga

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
 import { resolve } from "path";
 import { config } from "dotenv";
+import { GraphQLServer } from "graphql-yoga";
 
 config({ path: resolve(__dirname, "../.env") });
 const PORT = process.env.PORT;
-console.log(PORT);
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+	Query: {
+		hello: (_, { name }) => `Hello ${name || "World"}`
+	}
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start({ port: PORT }, () =>
+	console.log(`Server is running on http://localhost:${PORT}`)
+);
