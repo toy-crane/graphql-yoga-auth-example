48e5815011f96041cbd6848cdc58a1ad233c119c
hanwool kim <alwaysfun2183@gmail.com>
2020-04-01 18:03:23 +0900

    init .env

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log("hello");
+import { resolve } from "path";
+import { config } from "dotenv";
+
+config({ path: resolve(__dirname, "../.env") });
+const PORT = process.env.PORT;
+console.log(PORT);
