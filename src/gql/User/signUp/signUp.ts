54a5c9be3b8f3f280230180c37bac909a4f6d124
hanwool kim <alwaysfun2183@gmail.com>
2020-04-02 21:36:25 +0900

    sign Up 시에 Token 생성하도록 추가

--- src/gql/User/signUp/signUp.ts
+++ src/gql/User/signUp/signUp.ts
@@ -1,16 +1,28 @@
 import * as bcrypt from "bcryptjs";
 import { prisma } from "../../../../generated/prisma-client";
+import generateJWT from "../../../utils/auth/generateJWT";
 
 export default {
   Mutation: {
     signUp: async (parent, { name, username, password, email }, ctx, info) => {
       const hashedPassword = await bcrypt.hash(password, 10);
-      return await prisma.createUser({
+      const userExists = await prisma.$exists.user({
+        OR: [{ email }, { username }]
+      });
+      if (userExists) {
+        return new Error("User Already exists");
+      }
+      const user = await prisma.createUser({
         name,
         username,
         email,
         password: hashedPassword
       });
+      const token = generateJWT(user.id, user.email);
+      return {
+        user,
+        token
+      };
     }
   }
 };
